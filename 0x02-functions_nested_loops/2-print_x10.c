571186decf69efc07c4ead3a4268e895f03d57e8
Nelson Okiki <nelsonokiki@gmail.com>
2023-02-21 17:12:54 -0800

    my first task

---
+++ 0x02-functions_nested_loops/2-print_x10.c
@@ -0,0 +1,17 @@
+nclude "main.h"
+
+/**
+* print_alphabet_x10 - prints alpha 10 times
+*/
+void print_alphabet_x10(void)
+{
+	int i;
+	char la;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (la = 'a'; la <= 'z'; la++)
+			_putchar(la);
+		_putchar('\n');
+	}
+}
